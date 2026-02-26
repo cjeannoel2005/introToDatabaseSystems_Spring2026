105224c4d94aa7c8f66a0fdbe58d9927cc4db401
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2026-02-25 23:47:54 -0500

    Update Assignment3.sql

--- Assignments/Assignment3.sql
+++ Assignments/Assignment3.sql
@@ -39,3 +39,39 @@ WHERE SKU_Description LIKE '__d@'
 
 /*2.34*/
 SELECT 
+
+/*2.35*/
+/* The difference is that the sum calculates the total result of all the rows by adding them together
+   but the count calculation show the list of all the items in the selected row
+ */
+
+/* 2.36*/
+SELECT WarehouseID , SUM(QuantityOnHand)
+FROM INVENTORY AS
+WHERE TotalItemOnHand = SUM(TotalItemOnHand)
+GROUP BY WarehouseID
+ORDER BY TotalItemOnHand DESC ;
+
+/* 2.27 */
+SELECT WarehouseIS , SUM(QuantityOnHand) EXCEPT SUM(QuantityOnHand) => 3
+FROM INVENTORY
+GROUP BY WarehouseID
+WHERE SUM(QuantityOnHand) = TotalItemOnHandLT3
+ORDER BY TotalItemOnHandLT3 DESC;
+
+/* 2.38 */
+SELECT WarehouseID EXCEPT SUM(QuantityOnHand) >=2 ,
+       SUM(QuantityOnHand) EXCEPT SUM(QuantityOnHand) HAVING >= 3
+GROUP BY WarehouseID
+WHERE SUM(QuantityOnHand) = TotalItemOnHandLT3
+ORDER BY TotalItemOnHandLT3 DESC ;
+
+/* 2.39 */
+/* i would've applied the having before the where because it you filter out the variables
+   first then the logic would be less confusing when it comes to renaming the variable TotalItemOnHand
+ */
+
+/* 2.40*/
+SELECT SKU, SKU_Description , WarehouseID , WarehouseCity , WarehouseState
+FROM INVENTORY JOIN WAREHOUSE
+WHERE SKU , SKU_Description, WarehouseID , WarehouseCity  WarehouseState = 'Atlanta', 'Bangor', 'chicago'
